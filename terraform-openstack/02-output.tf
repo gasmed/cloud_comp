3a9a5e168e7d57430d68d97ad132b8b743faf4a2
gasmed <47139104+gasmed@users.noreply.github.com>
2025-05-19 00:52:27 +0300

    Update 02-output.tf

--- terraform-openstack/02-output.tf
+++ terraform-openstack/02-output.tf
@@ -1,7 +1,5 @@
-# 02-output.tf
-# Выводим IP созданного инстанса
-
-output "server_ip" {
-  description = "IPv4 адрес инстанса moviebot"
-  value       = openstack_compute_instance_v2.moviebot.access_ip_v4
+output "servers" {
+  value = {
+    serverip = openstack_compute_instance_v2.orlov_server.access_ip_v4
+  }
 }
