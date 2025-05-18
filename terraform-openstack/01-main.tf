014c7b1ecca841417a78ad1ef5ed56cd21bfcda0
gasmed <47139104+gasmed@users.noreply.github.com>
2025-05-19 00:52:03 +0300

    Update 01-main.tf

--- terraform-openstack/01-main.tf
+++ terraform-openstack/01-main.tf
@@ -1,4 +1,3 @@
-# 01-main.tf
 terraform {
   required_version = ">= 0.14.0"
   required_providers {
@@ -9,39 +8,48 @@ terraform {
   }
 }
 
+
+# use environment variables
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs
 provider "openstack" {
-  auth_url    = "https://cloud.crplab.ru:5000"
+  auth_url = "https://cloud.crplab.ru:5000"
+  tenant_id = "a02aed7892fa45d0bc2bef3b8a08a6e9"
   tenant_name = "students"
   user_domain_name = "Default"
-  user_name   = var.user_name
-  password    = var.password
-  region      = "RegionOne"
+  # export TF_VAR_user_name=
+  user_name = var.user_name
+  # export TF_VAR_password=
+  password = var.password
+  region = "RegionOne"
 }
 
-# Security group для SSH
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_secgroup_v2
 resource "openstack_networking_secgroup_v2" "sg" {
-  name = "securuty-group-trfm"
+  name = "orlov-group-trfm"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "ssh" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/networking_secgroup_rule_v2
+resource "openstack_networking_secgroup_rule_v2" "sg_ssh_rule" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 22
+  port_range_max = 22
+  remote_ip_prefix = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.sg.id
 }
 
-# Инстанс
-resource "openstack_compute_instance_v2" "moviebot" {
-  name            = "orlov-moviebot-server"
-  image_name      = var.image_name
-  flavor_name     = var.server_flavor
-  key_pair        = var.key_pair
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_instance_v2
+resource "openstack_compute_instance_v2" "orlov_server" {
+  name = "orlov-server-trfm"
+  image_name = var.image_name
+  flavor_name = var.server_flavor
+  key_pair =  "orlov"
   security_groups = [openstack_networking_secgroup_v2.sg.id]
 
   network {
     name = var.network_name
   }
 }
+
+# https://github.com/diodonfrost/terraform-openstack-examples
